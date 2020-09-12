6ced360f00718d57c6069748f3cddca172399c18
Tejas Powar <tejasvpowar@gmail.com>
2020-09-12 15:00:55 +0530

    Updates

---
+++ precache-manifest.90ffb3b5d572ff1a9951e53774943a31.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e3c1ec489dfb18dcf42648c2fdbe42",
+    "url": "/Attendance_prj/index.html"
+  },
+  {
+    "revision": "df266243b078f0444d34",
+    "url": "/Attendance_prj/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "2f5b78d823ddaf8e33b6",
+    "url": "/Attendance_prj/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "df266243b078f0444d34",
+    "url": "/Attendance_prj/static/js/2.3c0ce836.chunk.js"
+  },
+  {
+    "revision": "9e81e617707513432f3da640fbf3bd82",
+    "url": "/Attendance_prj/static/js/2.3c0ce836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f5b78d823ddaf8e33b6",
+    "url": "/Attendance_prj/static/js/main.a8faf8e0.chunk.js"
+  },
+  {
+    "revision": "e97cf87d3e02c0aa958a",
+    "url": "/Attendance_prj/static/js/runtime-main.84c23962.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Attendance_prj/static/media/logo.5d5d9eef.svg"
+  }
+]);
